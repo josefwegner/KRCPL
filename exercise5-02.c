bff10bd5f1ff7e32c70cf06d517aa98fe9822521
Josef Wegner <josef.wegner@outlook.com>
2026-08-11 10:08:00 +0200

    Added initial copy for Exercise 5-2
    Signed-off-by: Josef Wegner <josef.wegner@outlook.com>

---
+++ exercise5-02.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#define SIZE 5
+
+int getch(void);
+void ungetch(int);
+int getint(int *);
+
+int main(void) {
+  int i, n, array[SIZE];
+
+  for (n = 0; n < SIZE && getint(&array[n]) != EOF; n++)
+       ;
+
+  for (i = 0; i < n; i++)
+       printf("%d: %d\n", i, array[i]);
+
+  return EXIT_SUCCESS;
+}
+
+/* getint:  get next integer from input into *pn */
+int getint(int *pn)
+{
+  int c, sign;
+
+  while (isspace(c = getch()))   /* skip white space */
+        ;
+    if (!isdigit(c) && c != EOF && c != '+' && c != '-') {
+        ungetch(c);    /* it's not a number */
+        return 0;
+    }
+    sign = (c == '-') ? -1 : 1;
+    if (c == '+' || c == '-')
+        if (!isdigit(c = getch())) {
+            if (c != EOF)
+              ungetch(c);
+            *pn = 0;
+            return sign == -1 ? '-' : '+';
+        }
+    for (*pn = 0; isdigit(c); c = getch())
+        *pn = 10 * *pn + (c - '0');
+    *pn *= sign;
+    if (c != EOF)
+        ungetch(c);
+    return c;
+}
+
+#define BUFSIZE 100
+
+static char buf[BUFSIZE];  /* buffer for ungetch */
+static int  bufp = 0;      /* next free position in buf */
+
+int getch(void) /* get a (possibly pushed back) character */
+{
+      return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c) /* push character back on input */
+{
+      if (bufp >= BUFSIZE)
+                printf("ungetch: too many characters\n");
+          else
+                    buf[bufp++] = c;
+}
+
