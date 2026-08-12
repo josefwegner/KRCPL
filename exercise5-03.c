66d45bb984011bf43a8f929402939abd26fb5b64
Josef Wegner <josef.wegner@outlook.com>
2026-08-12 21:05:44 +0200

    Implemented Exercise 5-3.
    Signed-off-by: Josef Wegner <josef.wegner@outlook.com>

--- exercise5-03.c
+++ exercise5-03.c
@@ -14,13 +14,10 @@ int main(void)
 }
 
 /* kr_strcat:  concatenate t to end of s; s must be big enough */
-void kr_strcat(char s[], char t[])
+void kr_strcat(char *s, char *t)
 {
-    int i, j;
-
-    i = j = 0;
-    while (s[i] != '\0')    /* find end of s */
-        i++;
-    while ((s[i++] = t[j++]) != '\0')    /* copy t */
+    while (*s)    /* find end of s */
+        s++;
+    while ((*s++ = *t++))    /* copy t */
         ;
 }
