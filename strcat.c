de3525404d594ebb6c4f9855b7023d42d8caf663
Josef Wegner <josef.wegner@outlook.com>
2026-08-12 20:46:05 +0200

    Added main method and fixed issues.
    Signed-off-by: Josef Wegner <josef.wegner@outlook.com>

--- strcat.c
+++ strcat.c
@@ -1,11 +1,26 @@
-/* strcat:  concatenate t to end of s; s must be big enough */
-void strcat(char s[], char t[])
+#include <stdio.h>
+#include <stdlib.h>
+
+void kr_strcat(char [], char []);
+
+int main(void)
+{
+    char s[15] = "Hello, ",  t[] = "world!\n";
+
+    kr_strcat(s, t);
+    printf("%s", s);
+
+    return EXIT_SUCCESS;
+}
+
+/* kr_strcat:  concatenate t to end of s; s must be big enough */
+void kr_strcat(char s[], char t[])
 {
     int i, j;
 
     i = j = 0;
-    while (s[i] != ′\0′)    /* find end of s */
+    while (s[i] != '\0')    /* find end of s */
         i++;
-    while ((s[i++] = t[j++]) != ′\0′)    /* copy t */
+    while ((s[i++] = t[j++]) != '\0')    /* copy t */
         ;
 }
